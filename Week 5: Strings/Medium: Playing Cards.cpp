d7e92f74cf328feb77d8da6831ebb98d29888e2b
Shubham Kumar Yadav <75082349+ShubhamYv@users.noreply.github.com>
2022-06-26 18:49:29 +0530

    Create Medium: Playing Cards.cpp

---
+++ Week 5: Strings/Medium: Playing Cards.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+  
+void playing_cards()
+  {
+    //write your code here
+    int c=0,i;
+    string s,s1;
+    cin>>s;
+    for(i=0; i<5; i++)
+    {
+        cin>>s1;
+        if((s[0]==s1[0])||(s[1]==s1[1]))
+            c++;
+    }
+    if(c>0)
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
+  }
+
+int main(){
+  int n;
+  cin>>n;
+  for(int i=0; i<n; i++){
+    playing_cards();
+  }
+}
